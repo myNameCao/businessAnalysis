e4f38b6381ceb15ab799b1229498ad55b2ab9120
caohefei <hefei.cao@xiaoyangedu.com>
2021-07-26 14:08:02 +0800

    add module

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const { chdir } = require('process')
-
-const { execSync } = require('child_process')
-
-Promise.resolve('test')
-  .then(name => chdir(`/Users/caohefei/work/${name}`))
-  .then(() => {
-    return execSync('git pull origin develop')
-  })
-  .then(res => {
-    console.log(res.toString())
-  })
-  .then(() => {
-    return execSync('git log -1')
-  })
-  .then(res => {
-    console.log(res.toString())
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// 模块的 exports 对象 未完成的副本。（因此是不会陷入死循环的）
+// 然后 b.js 完成加载之后将 exports 对象提供给了 a.js 模块
+;(function (exports, require, module, __filename, __dirname) {
+  // 模块的代码
+})
---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+console.log(fs);
