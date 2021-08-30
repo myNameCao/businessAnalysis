61541986a9bc3dbe215e4eb4f7ffa7c238d94882
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 13:27:05 +0800

    sdd

---
+++ scripts/release.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const fs = require('fs')
+
+function updatePackage(version) {
+  const pkgPath = path.resolve('./', 'package.json')
+  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+  pkg.version = version
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+}
+
+updatePackage('3.0.0')
