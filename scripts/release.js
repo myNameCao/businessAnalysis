3892da3732bcef56dac5b0ac6e18b09b97236be2
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 15:28:31 +0800

    fix: 11212

--- scripts/release.js
+++ scripts/release.js
@@ -3,16 +3,16 @@ const fs = require('fs')
 const { execSync } = require('child_process')
 let version = process.argv[2] || ''
 
-updatePackage(version)
+// updatePackage(version)
 
 execSync(`git tag 'v${version}' && git push origin 'v${version}'`)
 
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${new Date().toDateString()}' && git push`
 )
-function updatePackage(version) {
-  const pkgPath = path.resolve('./', 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
-  pkg.version = version
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
-}
+// function updatePackage(version) {
+//   const pkgPath = path.resolve('./', 'package.json')
+//   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+//   pkg.version = version
+//   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+// }
