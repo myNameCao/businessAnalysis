9dd40bacc6c09c647eb85661aa9530de5b587965
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 17:40:05 +0800

    fix: dd

--- scripts/release.js
+++ scripts/release.js
@@ -5,7 +5,7 @@ const { execSync } = require('child_process')
 let version = process.argv[2] || ''
 updatePackage(version)
 
-execSync(`git tag 'a-${version}' && git push origin 'a-${version}' `)
+execSync(`git tag 'v-${version}' && git push origin 'v-${version}' `)
 
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${version}' && git push`
