518ee7b44234419dd0cf4981a7c852fdd8dfd66d
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 18:17:35 +0800

    fix: sr233

--- scripts/release.js
+++ scripts/release.js
@@ -8,7 +8,6 @@ updatePackage(version)
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${version}' && git push`
 )
-
 execSync(`git tag 'v-${version}' && git push origin --tags `)
 function updatePackage(version) {
   const pkgPath = path.resolve('./', 'package.json')
