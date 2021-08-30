f85516f077b8789b32df78edc36b9f69f7ff4d36
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 15:40:11 +0800

    fix: ddwwwew

--- scripts/release.js
+++ scripts/release.js
@@ -6,7 +6,7 @@ let version = process.argv[2] || ''
 
 updatePackage(version)
 
-execSync(`git tag '111' `)
+execSync(`git tag '${version}' `)
 
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${new Date().toDateString()}' && git push`
