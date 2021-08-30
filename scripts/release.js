f0221747dbdcbc55d672a8ea5f2839c83fda6f10
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 16:16:05 +0800

    fix: 3.1.2

--- scripts/release.js
+++ scripts/release.js
@@ -11,7 +11,6 @@ execSync(`git tag 'v-${version}'`)
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${version}' && git push`
 )
-
 function updatePackage(version) {
   const pkgPath = path.resolve('./', 'package.json')
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
