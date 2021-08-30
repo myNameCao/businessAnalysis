dc761bd0f8c25e5d4463169e43a66b1132af1257
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 15:08:30 +0800

    feat: 22

--- scripts/release.js
+++ scripts/release.js
@@ -8,10 +8,10 @@ let version = process.argv[2] || ''
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${new Date().toDateString()}' && git push`
 )
-updatePackage(version)
-function updatePackage(version) {
-  const pkgPath = path.resolve('./', 'package.json')
-  const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
-  pkg.version = version
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
-}
+// updatePackage(version)
+// function updatePackage(version) {
+//   const pkgPath = path.resolve('./', 'package.json')
+//   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
+//   pkg.version = version
+//   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n')
+// }
