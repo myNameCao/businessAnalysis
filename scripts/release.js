c7d87ad593b2440aa4d80f33766ab7c1fa0a3f04
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 14:18:12 +0800

    fix: add tag

--- scripts/release.js
+++ scripts/release.js
@@ -1,18 +1,14 @@
 const path = require('path')
 const fs = require('fs')
 const { execSync } = require('child_process')
-
 console.log(process.argv[2])
 let version = process.argv[2] || ''
-
 execSync(`git tag 'v${version}' && git push origin 'v${version}'  `)
 
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${new Date().toDateString()}' && git push`
 )
-
 updatePackage(version)
-
 function updatePackage(version) {
   const pkgPath = path.resolve('./', 'package.json')
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
