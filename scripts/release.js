06cc9082697fa093f8f284aec71ff899952a999a
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 18:19:00 +0800

    fix: 23232

--- scripts/release.js
+++ scripts/release.js
@@ -8,7 +8,7 @@ updatePackage(version)
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${version}' && git push`
 )
-execSync(`git tag 'v-${version}' && git push origin --tags `)
+execSync(`git tag '${version}' && git push origin --tags `)
 function updatePackage(version) {
   const pkgPath = path.resolve('./', 'package.json')
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
