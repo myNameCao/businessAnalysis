cc31384bbdb3bd01a59f9d1fe8c46802edbd65fc
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 16:24:24 +0800

    3.1.7

--- scripts/release.js
+++ scripts/release.js
@@ -6,7 +6,7 @@ let version = process.argv[2] || ''
 
 updatePackage(version)
 
-execSync(`git tag 344343 `)
+execSync(`git tag '344sdfsfsdf343' `)
 
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${version}' && git push`
