39a25bffe0b6aefc327e4a57d88d2943146c7ef8
caohefei <hefei.cao@xiaoyangedu.com>
2021-08-30 15:26:23 +0800

    fix: version

--- scripts/release.js
+++ scripts/release.js
@@ -3,12 +3,13 @@ const fs = require('fs')
 const { execSync } = require('child_process')
 let version = process.argv[2] || ''
 
+updatePackage(version)
+
 execSync(`git tag 'v${version}' && git push origin 'v${version}'`)
 
 execSync(
   `yarn changelog && git add . && git commit -m 'release(自动化): ${new Date().toDateString()}' && git push`
 )
-updatePackage(version)
 function updatePackage(version) {
   const pkgPath = path.resolve('./', 'package.json')
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
