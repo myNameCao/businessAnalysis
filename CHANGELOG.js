a5662185742ba932a558b675c43f2701e4a31ab3
caohefei <hefei.cao@xiaoyangedu.com>
2021-09-03 10:38:12 +0800

    release(自动化): 1.1.12

--- CHANGELOG.js
+++ CHANGELOG.js
@@ -1,3 +1,12 @@
+## [1.1.11](https://github.com/myNameCao/node/compare/1.1.10...1.1.11) (2021-09-03)
+
+
+### Bug Fixes
+
+* test ([f783289](https://github.com/myNameCao/node/commit/f7832891ee4fd2a6b1ad1d05dd01a3b44eeaf9c6))
+
+
+
 ## [1.1.10](https://github.com/myNameCao/node/compare/1.0.9...1.1.10) (2021-09-03)
 
 
