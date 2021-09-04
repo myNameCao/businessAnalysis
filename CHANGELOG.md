ab74a294605be171e5364c378e58b290ff70ec35
caohefei <hefei.cao@xiaoyangedu.com>
2021-09-04 09:02:54 +0800

    fix: test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,4 @@
-## [4.0.33](https://github.com/myNameCao/node/compare/4.0.0...4.0.33) (2021-09-04)
-
-
-### Bug Fixes
-
-* test ([a2a62d9](https://github.com/myNameCao/node/commit/a2a62d9a581d9b9701120dcaa89ea1b39d2a64a2))
+## [3.9.44](https://github.com/myNameCao/node/compare/3.9.9...3.9.44) (2021-09-04)
 
 
 
--- scripts/release.js
+++ scripts/release.js
@@ -3,9 +3,13 @@ const fs = require('fs')
 const { execSync } = require('child_process')
 let version = process.argv[2] || ''
 updatePackage(version)
+
+execSync(`yarn changelog `)
 // 必须都是同步的
-execSync(`yarn changelog`)
-execSync(`git tag '${version}' && git push origin --tags `)
+// execSync(
+//   `yarn changelog && git add . && git commit -m 'release(自动化): ${version}' && git push`
+// )
+// execSync(`git tag '${version}' && git push origin --tags `)
 function updatePackage(version) {
   const pkgPath = path.resolve('./', 'package.json')
   const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
