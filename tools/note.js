03bef005b9c475b1d657cef8bd8e0ac1eea98c9d
caohefei <hefei.cao@xiaoyangedu.com>
2021-09-04 18:49:44 +0800

    fix: dddd

--- tools/note.js
+++ tools/note.js
@@ -22,7 +22,7 @@ const note = async name => {
         {
           markdown: {
             title: name + '  发版完成',
-            text
+            text: `# ${name} 发版成功 \n> ${text}  `
           },
           headers: { 'Content-Type': 'application/json' },
           msgtype: 'markdown'
