c43a7311b54b36824b7418e3af991eeae71b45a7
caohefei <hefei.cao@xiaoyangedu.com>
2021-09-29 22:16:13 +0800

    chore: style

--- tools/pubish.js
+++ tools/pubish.js
@@ -108,11 +108,11 @@ const task = name => {
   return funs()
     .then(() => {
       spinner.succeed(`${name} 发布完成`)
-      note(name, changelogText, '成功 👏 👏 👏 👏 👏 👏 👏 ')
+      note(name, changelogText, '👏 👏 👏 👏 👏 👏 👏 ')
     })
     .catch(err => {
       spinner.fail(err + '    ' + name)
-      note(name, '发版失败😓', ' 失败 😵‍💫 😵‍💫 😵‍💫 ')
+      note(name, ' ', ' 失败 😵‍💫 😵‍💫 😵‍💫 ')
     })
 }
 
