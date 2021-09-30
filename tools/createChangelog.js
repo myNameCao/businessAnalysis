249f0dabef7936a36d44b126bbce2e2543be12dd
caohefei <hefei.cao@xiaoyangedu.com>
2021-09-30 10:49:39 +0800

    perf: 优化代码

--- tools/createChangelog.js
+++ tools/createChangelog.js
@@ -3,6 +3,7 @@ const conventionalChangelog = require('conventional-changelog')
 const { chdir } = require('process')
 
 const changeLog = async name => {
+  // 注意 必须a
   chdir(`/Users/caohefei/work/${name}`)
   let stream = conventionalChangelog({
     preset: 'angular'
--- tools/note.js
+++ tools/note.js
@@ -4,16 +4,16 @@ const url =
 const project = {
   test_new: '考试安排',
   report: '过程与阶段性报告',
-  base: '教学数据|设置',
+  base: '教学数据|公共设置',
   node: '测试项目',
-  exam: '题库|教辅系统',
+  exam: '题库组卷|教辅系统',
   mark: '在线阅卷',
   datacenter: '学情分析',
   check: '晓羊策学——公众号',
   center: '后台管理系统',
   unionexam: '区域联考',
   pay: '支付列表',
-  answersheet: '答题卡'
+  answersheet: '答题卡制作'
 }
 
 const note = (name, text, emoji) => {
