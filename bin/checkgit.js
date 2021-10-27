cd27bc81c312134fe5e2336a6ec8d823a24ab804
caohefei <hefei.cao@xiaoyangedu.com>
2021-10-27 16:40:28 +0800

    feat: observe_publish

--- bin/checkgit.js
+++ bin/checkgit.js
@@ -1,5 +1,4 @@
-// git describe --tags `git rev-list --tags --max-count=1`
-
+#!/usr/bin/env node
 const { composeAsync } = require('../composeAsync')
 
 const chalk = require('chalk')
@@ -7,7 +6,6 @@ const chalk = require('chalk')
 const { execSync } = require('child_process')
 
 const { chdir } = require('process')
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require('constants')
 
 let PATH = '/Users/caohefei/work'
 
@@ -17,18 +15,24 @@ let check = name => {
       chdir(`${PATH}/${name}`)
     })
     .then(() => {
-      execSync('git pull origin develop')
+      return execSync('git pull origin develop')
+    })
+    .then(res => {
+      console.log(res.toString())
     })
     .then(() => {
       let endtag = execSync(`git rev-list --tags --max-count=1`)
-      let head = execSync(`git rev-parse HEAD`)
-      if (endtag.toString() !== head.toString()) {
-        console.error(`${chalk.bgRed.white(name)} ${chalk.red('有变动')}`)
-        return execSync(`git diff ${head}`)
-      }
+        .toString()
+        .trim() // 最后的tag 的commitId
+      let head = execSync(`git rev-parse HEAD`).toString().trim() // 最后一次提交
+      let txt = execSync(`git log ${endtag}..HEAD`) //buffer
+      return { txt, endtag, head }
     })
-    .then(res => {
-      //   console.log(res.toString())
+    .then(({ txt, endtag, head }) => {
+      if (endtag !== head) {
+        console.log(`${chalk.bgRed.white(name)} ${chalk.red('改动内容如下')}`)
+        console.log(txt.toString())
+      }
     })
 }
 
