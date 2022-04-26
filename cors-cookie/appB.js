3e8d46fc183c74d295bc1168e4fa2bb72cf4b752
caohefei <hefei.cao@xiaoyangedu.com>
2022-04-26 16:44:03 +0800

    fix: cors 跨域 携带 cookie

--- cors-cookie/appB.js
+++ cors-cookie/appB.js
@@ -19,8 +19,8 @@ router.get('/anotherService', ctx => {
 
 app
   .use(async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*')
-    // ctx.set('Access-Control-Allow-Credentials', 'true')
+    ctx.set('Access-Control-Allow-Origin', 'http://localhost:8000')
+    ctx.set('Access-Control-Allow-Credentials', 'true')
     ctx.set(
       'Access-Control-Allow-Headers',
       'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild'
