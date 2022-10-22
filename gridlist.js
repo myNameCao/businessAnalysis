c84ba2811e95e86512ddf8953ccd6ff130a9b9ec
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-10-22 19:39:36 +0800

    feat: 数据

--- gridlist.js
+++ gridlist.js
@@ -1,31 +1,17 @@
 const { stock } = require('tushare')
 
-const options = {
-  code: '603078',
-  start: '2022-1-11'
-}
-
-const getHistory = () => {
+const getHistory = options => {
   stock.getHistory(options).then(({ data }) => {
-    let list = data.record.filter(item => {
+    let list = data?.record || []
+    list = list.filter(item => {
       return new Date(item[0]).getTime() > new Date(options.start).getTime()
     })
     console.log(list.length)
-    console.log(list)
+    console.log(list.pop())
   })
 }
 
-// stock.getHS300().then(({ data }) => {
-//   console.log(data)
-//   console.log(data.length)
-// })
-
-// stock.getHS300().then(({ data }) => {
-//   console.log(data)
-//   console.log(data.length)
-// })
-
-let num = 0
+let num = []
 // 获取行业信息
 // {
 //   tag: 'new_blhy',
@@ -44,17 +30,17 @@ let num = 0
 // }
 
 const getSinaIndustryClassified = () => {
-  num = 0
+  num = []
   stock.getSinaIndustryClassified().then(async ({ data }) => {
     for (let a = 0; a < data.length; a++) {
       await getSinaClassifyDetails(data[a])
     }
-    console.log(('共', num))
+    console.log('共', num.length)
+    console.log(num)
   })
 }
 
 // 获取行业 包含的股票信息
-
 //{
 //   symbol: 'sz000571',
 //   name: 'ST大洲',
@@ -72,14 +58,27 @@ const getSinaClassifyDetails = async classObj => {
   var options1 = {
     tag: classObj.tag
   }
-  return await stock.getSinaClassifyDetails(options1).then(async ({ data }) => {
-    console.log(classObj.name, data.length, '只')
-    for (let b = 0; b < data.length; b++) {
-      let item = data[b]
-      num++
-      console.log(item.name, item.symbol, item.price)
-    }
-  })
+  return await stock
+    .getSinaClassifyDetails(options1)
+    .then(({ data }) => {
+      console.log(
+        classObj.name,
+        classObj.tag,
+        '=========================================',
+        data.length,
+        '只'
+      )
+      for (let b = 0; b < data.length; b++) {
+        let item = data[b]
+        num.push({ code: item.symbol, name: item.name })
+      }
+    })
+    .catch(e => {
+      console.log(e)
+    })
 }
-
+// 按行业查询
 getSinaIndustryClassified()
+
+// 获得具体的信息
+// getHistory()
