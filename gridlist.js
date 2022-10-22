828fe486a1c979041b4042652b546e926c9ee6cd
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-10-22 13:20:35 +0800

    fix: merhe cde

--- gridlist.js
+++ gridlist.js
@@ -27,33 +27,59 @@ const getHistory = () => {
 
 let num = 0
 // 获取行业信息
+// {
+//   tag: 'new_blhy',
+//   name: '玻璃行业',
+//   num: '19',
+//   price: '15.805789473684',
+//   changePrice: '-0.062105263157895',
+//   changePercent: '-0.39138943248532',
+//   volume: 2368517.03,
+//   amount: 249027.6675,
+//   leadingSymbol: 'sz300160',
+//   leadingChangePercent: '1.906',
+//   leadingPrice: '6.950',
+//   leadingChangePrice: '0.130',
+//   leadingName: '秀强股份'
+// }
+
 const getSinaIndustryClassified = () => {
   num = 0
-  stock.getSinaIndustryClassified().then(({ data }) => {
-    console.log(data)
-    // data.forEach(element => {
-    //   // getSinaClassifyDetails(element)
-    // })
+  stock.getSinaIndustryClassified().then(async ({ data }) => {
+    for (let a = 0; a < data.length; a++) {
+      await getSinaClassifyDetails(data[a])
+    }
+    console.log(('共', num))
   })
 }
 
-// 获取行业 包含 的股票信息
-const getSinaClassifyDetails = classObj => {
-  console.log(classObj)
+// 获取行业 包含的股票信息
 
+//{
+//   symbol: 'sz000571',
+//   name: 'ST大洲',
+//   price: '2.600',
+//   changePrice: -0.04,
+//   changePercent: -1.515  ,
+//   open: '2.630',
+//   high: '2.680',
+//   low: '2.550',
+//   volume: 167347.25,
+//   amount: 4389.8841,
+//   tickTime: '15:00:00'
+//   }
+const getSinaClassifyDetails = async classObj => {
   var options1 = {
     tag: classObj.tag
   }
-  stock.getSinaClassifyDetails(options1).then(({ data }) => {
-    console.log(classObj.tag, ':', data.length, '只')
-    data.forEach(i => {
+  return await stock.getSinaClassifyDetails(options1).then(async ({ data }) => {
+    console.log(classObj.name, data.length, '只')
+    for (let b = 0; b < data.length; b++) {
+      let item = data[b]
       num++
-
-      console.log(i.name, i.symbol)
-    })
+      console.log(item.name, item.symbol, item.price)
+    }
   })
-
-  console.log(num)
 }
 
 getSinaIndustryClassified()
