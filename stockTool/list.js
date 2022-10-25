129f1de9b020c0231c595bd4339911379052a92b
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-10-25 14:48:12 +0800

    chore: merge code

--- stockTool/list.js
+++ stockTool/list.js
@@ -1,3 +1,5 @@
-let  list = [1,1]
-  exports.list = list
-  
+let list = [
+  { name: '京城股份', symbol: 'sh600860', price: '13.930' },
+  { name: '中来股份', symbol: 'sz300393', price: '16.160' }
+]
+exports.list = list
