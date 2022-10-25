915dfb227403fec18d4c712fe1f76450dfbf2593
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-10-25 14:57:44 +0800

    chore: merge code

--- stockTool/list.js
+++ stockTool/list.js
@@ -1,5 +1,3 @@
-let list = [
-  { name: '京城股份', symbol: 'sh600860', price: '13.930' },
-  { name: '中来股份', symbol: 'sz300393', price: '16.160' }
-]
-exports.list = list
+let  list = [{"name":"佳力图","symbol":"sh603912","price":"10.340"},{"name":"中来股份","symbol":"sz300393","price":"16.160"},{"name":"京城股份","symbol":"sh600860","price":"13.890"}]
+  exports.list = list
+  
--- stockTool/mylike.js
+++ stockTool/mylike.js
@@ -1,7 +1,12 @@
 let { getHistory } = require('./getHistory')
 
-let { list } = require('./list')
+let { writeFile } = require('./wirter')
 
+let { list } = require('./list')
+writeFile(
+  '                                            日期: ' +
+    new Date().toLocaleDateString()
+)
 const testList = async () => {
   for (let i = 0; i < list.length; i++) {
     let item = list[i]
