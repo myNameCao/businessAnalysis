c73c550a222563e8b40d5b7d587cb7f4ac3b2f2b
hefei.cao <hefei.cao@xiaoyangedu.com>
2023-02-27 15:42:53 +0800

    chore: 添加验证文件

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: hefei.cao hefei.cao@xiaoyangedu.com
+ * @Date: 2023-02-27 15:41:48
+ * @LastEditors: hefei.cao hefei.cao@xiaoyangedu.com
+ * @LastEditTime: 2023-02-27 15:42:07
+ * @FilePath: \businessAnalysis\scripts\verifyCommit.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // Invoked on the commit-msg git hook by yorkie.
 
 const chalk = require('chalk')
