3f3150a6f62851e5057394cfb54438f5b2d02dbc
hefei.cao <hefei.cao@xiaoyangedu.com>
2023-03-29 13:25:52 +0800

    chore: merge code

--- stockTool/msg.js
+++ stockTool/msg.js
@@ -1,11 +1,11 @@
 let msg = {
-  p_change: -2,
-  last_day: [5, 3],
+  p_change: -10,
+  last_day: [5, 0],
   max_min_prices: [30, 5],
-  band: 0.2, // 起伏 越小越多
-  diff: 0.15, //低差 越大越多
-  bandNumber: 4, //起伏次数
-  activeNumber: 15, //默认值 15 达标 7%
+  band: 0.2, // 越小越多
+  diff: 0.15, // 越大越多
+  bandNumber: 4, //次数
+  activeNumber: 15, //默认值 15  7%
   noteList: [],
   temp: {},
   checktemp: {},
