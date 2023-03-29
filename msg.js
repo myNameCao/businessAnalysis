9e41e46963c3bea7ef543f7f0d5bf7ee500b7c4c
hefei.cao <hefei.cao@xiaoyangedu.com>
2023-03-29 17:12:52 +0800

    feat: 按行业排序

--- msg.js
+++ msg.js
@@ -2,8 +2,8 @@ let msg = {
   p_change: 0,
   last_day: [5, 0],
   max_min_prices: [30, 5],
-  band: 0.3, // 越小越多
-  diff: 0.15, // 越大越多
+  band: 0.3, // 趋向小
+  diff: 0.15, // 趋向大
   bandNumber: 4, //次数
   onMacd_Kdj: false,
   activeNumber: 15, //默认值 15  7%
