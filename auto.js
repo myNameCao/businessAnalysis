fd8a5c070625a13862fc74d3e1a4877ffa0bfb88
caohefei <hefei.cao@xiaoyangedu.com>
2023-04-24 05:54:40 +0800

    chore: merge code

---
+++ auto.js
@@ -0,0 +1,4 @@
+launch('weixin')
+
+//启动微信
+app.launchApp('i茅台')
--- stockTool/yuyue.js
+++ stockTool/yuyue.js
@@ -1,20 +1,30 @@
 const axios = require('axios')
 
-const token = '031Vbt0001ICPP12TF200VGFuJ0Vbt0D'
+//
+
+// {
+// 	"code": "000",
+// 	"message": "success",
+// 	"data": [{
+// 		"V": "黑龙江公司",
+// 		"K": "MT_HLJHEB1001"
+// 	}]
+// }
+
 const headers = {
   Host: 'reserve.moutai.com.cn',
-  Accept: 'application/json, text/plain, */*',
-  'Cache-Control': 'no-cache',
-  'User-Agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/6.8.0(0x16080000) MacWechat/3.7.1(0x1307010b) XWEB/30414 Flue',
+  Accept:
+    'application/json, text/pla 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/6.8.0(0x16080000) MacWechat/3.7.1(0x1307010b) XWEB/30414 Flue',
   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
   Origin: 'https://reserve.moutai.com.cn',
   'Sec-Fetch-Site': 'same-origin',
   'Sec-Fetch-Mode': 'cors',
   'Sec-Fetch-Dest': 'empty',
-  Referer: `https://reserve.moutai.com.cn/mconsumer/?a=1&token=${token}`,
+  Referer:
+    'https://reserve.moutai.com.cn/mconsumer/?a=1&token=0716dr000jAiQP14az300EmI4m16dr0Y',
   'Accept-Language': 'zh-CN,zh',
-  Cookie: `lambo-sso-key_0_=${token}#6Xy5fCzePSsn/7ZyhCkFk2zSApYp+DLOnu7BxonpVfw=`
+  Cookie:
+    'lambo-sso-key_0_=0716dr000jAiQP14az300EmI4m16dr0Y#Kdd1iK3EB5x9xASir5pOJuSvSVGvbMapibv/AGtYMfg='
 }
 
 const url =
@@ -57,8 +67,36 @@ const getShops = () => {
     })
 }
 
-getShops()
+// getShops()
 
-setInterval(() => {
-  getShops()
-}, 1000 * 60 * 1)
+const yuyue = () => {
+  const response = axios
+    .get(
+      'https://reserve.moutai.com.cn/api/rsv-server/anon/manage/rimItem/helpBox',
+      {
+        params: {
+          shopId: 'MT_HLJHEB1001'
+        },
+        headers: {
+          Host: 'reserve.moutai.com.cn',
+          Accept: 'application/json, text/plain, */*',
+          'Cache-Control': 'no-cache',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/6.8.0(0x16080000) MacWechat/3.7.1(0x1307010b) XWEB/30414 Flue',
+          'Sec-Fetch-Site': 'same-origin',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Dest': 'empty',
+          Referer:
+            'https://reserve.moutai.com.cn/mconsumer/?a=1&token=0716dr000jAiQP14az300EmI4m16dr0Y',
+          'Accept-Language': 'zh-CN,zh',
+          Cookie:
+            'lambo-sso-key_0_=0716dr000jAiQP14az300EmI4m16dr0Y#Kdd1iK3EB5x9xASir5pOJuSvSVGvbMapibv/AGtYMfg='
+        }
+      }
+    )
+    .then(res => {
+      console.log(res.data)
+    })
+}
+
+yuyue()
